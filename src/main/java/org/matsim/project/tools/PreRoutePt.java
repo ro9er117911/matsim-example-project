a3e4dab174fae834bd5258b1060d35093939d118
ro9er117911 <rogerlo117911@gmail.com>
2025-10-21 16:53:39 +0800

    Refactor code structure for improved readability and maintainability

--- src/main/java/org/matsim/project/tools/PreRoutePt.java
+++ src/main/java/org/matsim/project/tools/PreRoutePt.java
@@ -4,14 +4,15 @@ import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.controler.ControllerUtils;
 import org.matsim.core.population.algorithms.XY2Links;
 import org.matsim.core.population.io.PopulationWriter;
 import org.matsim.core.router.PlanRouter;
+import org.matsim.core.router.TripRouter;
+import org.matsim.core.router.TripRouterFactoryBuilderWithDefaults;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.timing.TimeInterpretation;
 
-import com.google.inject.Injector;
+import jakarta.inject.Provider;
 
 public final class PreRoutePt {
 
@@ -29,8 +30,10 @@ public final class PreRoutePt {
 
 		new XY2Links(scenario).run(scenario.getPopulation());
 
-		final Injector injector = ControllerUtils.createAdhocInjector(config, scenario);
-		final PlanRouter planRouter = injector.getInstance(PlanRouter.class);
+		final Provider<TripRouter> tripRouterProvider = TripRouterFactoryBuilderWithDefaults
+				.createDefaultTripRouterFactoryImpl(scenario);
+		final TripRouter tripRouter = tripRouterProvider.get();
+		final PlanRouter planRouter = new PlanRouter(tripRouter, TimeInterpretation.create(config));
 
 		for (Person person : scenario.getPopulation().getPersons().values()) {
 			planRouter.run(person.getSelectedPlan());

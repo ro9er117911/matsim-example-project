208128e0ff93851363a28c7c6e8f60531793267a
ro9er117911 <rogerlo117911@gmail.com>
2025-10-21 16:36:52 +0800

    Add transit schedule and vehicle definitions for bus and subway services
    - Created a new transit schedule XML file defining bus routes and stops, including:
      - Two bus stops: "臺北科技大學(忠孝)" and "捷運忠孝復興站(公車)"
      - Two transit routes (BUS_EB and BUS_WB) with specified departure times and stop profiles.
    
    - Added a new transit vehicles XML file defining vehicle types for buses and subways, including:
      - Attributes for access and egress times, capacity, dimensions, and network modes.
      - Defined two bus vehicles (veh_0_bus and veh_1_bus) based on the Bus vehicle type.

---
+++ src/main/java/org/matsim/project/tools/PreRoutePt.java
@@ -0,0 +1,42 @@
+package org.matsim.project.tools;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.ControllerUtils;
+import org.matsim.core.population.algorithms.XY2Links;
+import org.matsim.core.population.io.PopulationWriter;
+import org.matsim.core.router.PlanRouter;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.timing.TimeInterpretation;
+
+import com.google.inject.Injector;
+
+public final class PreRoutePt {
+
+	private PreRoutePt() {
+	}
+
+	public static void main(final String[] args) {
+		final String configFile = args.length > 0 ? args[0] : "scenarios/equil/config.xml";
+		final String outPlans = args.length > 1 ? args[1] : "scenarios/equil/population_routed.xml";
+
+		final Config config = ConfigUtils.loadConfig(configFile);
+		config.transit().setUseTransit(true);
+
+		final Scenario scenario = ScenarioUtils.loadScenario(config);
+
+		new XY2Links(scenario).run(scenario.getPopulation());
+
+		final Injector injector = ControllerUtils.createAdhocInjector(config, scenario);
+		final PlanRouter planRouter = injector.getInstance(PlanRouter.class);
+
+		for (Person person : scenario.getPopulation().getPersons().values()) {
+			planRouter.run(person.getSelectedPlan());
+		}
+
+		new PopulationWriter(scenario.getPopulation()).write(outPlans);
+		System.out.println("Wrote routed plans to: " + outPlans);
+	}
+}
